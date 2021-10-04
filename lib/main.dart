c70ec7e1d1a9fdcc17be955ae4acec1fa2e890c3
klivi <klivensziu3@gmail.com>
2021-10-04 13:16:55 +0200

    ADD visual plus buttonplaceholder, will redirect to Pick Playlist screen WIP

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:assets_audio_player/assets_audio_player.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -26,15 +25,16 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage>
     with SingleTickerProviderStateMixin {
-
   bool isPlaying = false;
 
   AssetsAudioPlayer audioPlayer = AssetsAudioPlayer();
   @override
   void initState() {
     super.initState();
-    audioPlayer.open(Audio('assets/sounds/lofi.mp3'),autoStart: false,showNotification: true);
+    audioPlayer.open(Audio('assets/sounds/lofi.mp3'),
+        autoStart: false, showNotification: true);
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,63 +44,95 @@ class _MyHomePageState extends State<MyHomePage>
           ),
           backgroundColor: Colors.deepPurple,
         ),
-        body: Container(
-          height: 210,
-          width: 210,
-          child: Stack(
-            children: <Widget>[
-              Card(
-                child: Image.asset(
-                  'assets/images/street-japan-night.jpg',
-                  fit: BoxFit.cover,
-                ),
-                clipBehavior: Clip.antiAliasWithSaveLayer,
-                elevation: 8,
-                // shadowColor: Colors.green,
-                margin: EdgeInsets.all(20),
-                shape: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: BorderSide(color: Colors.black, width: 0)),
-              ),
-              Positioned(
-                top: 25,
-                left: 130,
-                child: ElevatedButton(
-                  child: Icon(Icons.play_arrow),
-                  onPressed: () => playSound(),
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.black54,
-                    shape: CircleBorder(),
+        body: Row(
+          children: <Widget>[
+            Container(
+              height: 195,
+              width: 195,
+              child: Stack(
+                children: <Widget>[
+                  Card(
+                    child: Image.asset(
+                      'assets/images/street-japan-night.jpg',
+                      fit: BoxFit.cover,
+                    ),
+                    clipBehavior: Clip.antiAliasWithSaveLayer,
+                    elevation: 8,
+                    // shadowColor: Colors.green,
+                    margin: EdgeInsets.fromLTRB(20, 20, 15, 20),
+                    shape: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide(color: Colors.black, width: 0)),
                   ),
-                ),
-              ),
-              Positioned(
-                top: 138,
-                left: 130,
-                child: ElevatedButton(
-                  child: Icon(Icons.more_vert_sharp),
-                  onPressed: () => print("MENU"),
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.black54,
-                    shape: CircleBorder(),
+                  Positioned(
+                    top: 24,
+                    left: 115,
+                    child: ElevatedButton(
+                      child: Icon(Icons.play_arrow),
+                      onPressed: () => playSound(),
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.black54,
+                        shape: CircleBorder(),
+                      ),
+                    ),
                   ),
-                ),
+                  Positioned(
+                    top: 123,
+                    left: 115,
+                    child: ElevatedButton(
+                      child: Icon(Icons.more_vert_sharp),
+                      onPressed: () => print("MENU"),
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.black54,
+                        shape: CircleBorder(),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+            Container(
+              height: 195,
+              width: 195,
+              child: Stack(
+                children: <Widget>[
+                  Card(
+                    clipBehavior: Clip.antiAliasWithSaveLayer,
+                    elevation: 7,
+                    color: Colors.blue,
+                    // shadowColor: Colors.green,
+                    margin: EdgeInsets.fromLTRB(15, 20, 20, 20),
+                    shape: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide(color: Colors.black, width: 1)),
+                  ),
+                  Center(
+                    child: ElevatedButton(
+                      child: Icon(Icons.add_box_rounded),
+                      onPressed: () => print("MENU"),
+                      style: ElevatedButton.styleFrom(
+                        fixedSize: Size(60, 60),
+                        primary: Colors.black54,
+                        shape: CircleBorder(),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
         backgroundColor: Color.fromRGBO(20, 25, 39, 1)
-      //bottomNavigationBar: ,
-    );
+        //bottomNavigationBar: ,
+        );
   }
 
   void playSound() {
     setState(() {
       isPlaying = !isPlaying;
-      if(isPlaying)
-      {
+      if (isPlaying) {
         audioPlayer.play();
-      }else{
+      } else {
         audioPlayer.pause();
       }
     });
