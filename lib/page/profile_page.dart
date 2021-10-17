70398d1aa96b133670f68da09209e1fa66761236
giannisalinierecoursy <gianni.saliniere-coursy@epitech.eu>
2021-10-17 12:46:41 +0200

    fix: camera picker

--- lib/page/profile_page.dart
+++ lib/page/profile_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_1/widget/footer.dart';
 import 'package:flutter_1/widget/icon/icon_widget.dart';
@@ -12,7 +14,7 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-  Future<XFile?>? photoTaken;
+  File? photoTaken;
 
   @override
   Widget build(BuildContext context) {
@@ -24,143 +26,152 @@ class _ProfilePageState extends State<ProfilePage> {
         centerTitle: true,
         backgroundColor: Theme.of(context).primaryColor,
       ),
-      body: SingleChildScrollView(child: 
-        Column(
-        children: [
-          // Align(
-          //   alignment: Alignment.topRight,
-          //   child: GestureDetector(
-          //     onTap: () {
-          //       Navigator.push(
-          //         context,
-          //         MaterialPageRoute(builder: (context) => UserStatisticsPage()),
-          //       );
-          //     },
-          //     child: Container(
-          //       height: 70,
-          //       width: 70,
-          //       decoration: BoxDecoration(
-          //         borderRadius: BorderRadius.circular(17),
-          //         color: Theme.of(context).cardColor,
-          //       ),
-          //       child: Align(
-          //           child: Text(
-          //         'Statistics',
-          //         style: TextStyle(color: Colors.white),
-          //       )),
-          //     ),
-          //   ),
-          // ),
-          SizedBox(
-            height: 20,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                height: 130,
-                width: 150,
-                decoration: BoxDecoration(
-                    color: Theme.of(context).cardColor,
-                    borderRadius: BorderRadius.circular(17)),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    if (photoTaken == null)
-                      Icon(
-                        Icons.account_circle,
-                        size: 80,
-                        color: Color.fromRGBO(142, 142, 147, 1),
-                      )
-                    else
-                      Text('$photoTaken'),
-                    Align(
-                      alignment: FractionalOffset.bottomCenter,
-                      child: Text(
-                        getText('tapToAdd').toString(),
-                        style: TextStyle(color: Colors.white, fontSize: 16),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-            height: 250,
-            width: 350,
-            padding: EdgeInsets.fromLTRB(20, 10, 0, 10),
-            decoration: BoxDecoration(
-                color: Theme.of(context).cardColor,
-                borderRadius: BorderRadius.circular(17)),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // Align(
+            //   alignment: Alignment.topRight,
+            //   child: GestureDetector(
+            //     onTap: () {
+            //       Navigator.push(
+            //         context,
+            //         MaterialPageRoute(builder: (context) => UserStatisticsPage()),
+            //       );
+            //     },
+            //     child: Container(
+            //       height: 70,
+            //       width: 70,
+            //       decoration: BoxDecoration(
+            //         borderRadius: BorderRadius.circular(17),
+            //         color: Theme.of(context).cardColor,
+            //       ),
+            //       child: Align(
+            //           child: Text(
+            //         'Statistics',
+            //         style: TextStyle(color: Colors.white),
+            //       )),
+            //     ),
+            //   ),
+            // ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(getText('personalInformation').toString(),
-                    style: TextStyle(color: Colors.white, fontSize: 23)),
-                Row(
-                  children: [
-                    Text(getText('firstName').toString() + ': Marine',
-                        style: TextStyle(color: Colors.white, fontSize: 20)),
-                  ],
+                GestureDetector(
+                  onTap: () {
+                    pickImageFromCamera(context: context);
+                  },
+                  child: Container(
+                    height: 130,
+                    width: 150,
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).cardColor,
+                        borderRadius: BorderRadius.circular(17)),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        photoTaken == null
+                            ? Icon(
+                                Icons.account_circle,
+                                size: 80,
+                                color: Color.fromRGBO(142, 142, 147, 1),
+                              )
+                            : SizedBox(
+                                height: 100,
+                                width: 100,
+                                child: Image.file(photoTaken!)),
+                        Align(
+                          alignment: FractionalOffset.bottomCenter,
+                          child: Text(
+                            getText('tapToAdd').toString(),
+                            style: TextStyle(color: Colors.white, fontSize: 16),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-                Row(
-                  children: [
-                    Text(getText('lastName').toString() + ': Lepen',
-                        style: TextStyle(color: Colors.white, fontSize: 20)),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Text(getText('username').toString() + ': lamarinade',
-                        style: TextStyle(color: Colors.white, fontSize: 20)),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Text(getText('email').toString() + ': marine.lepen@test.com',
-                        style: TextStyle(color: Colors.white, fontSize: 20)),
-                  ],
-                )
               ],
             ),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          buildLanguage(),
-          buildDarkMode(),
-          // GestureDetector(
-          //   onTap: () {
-          //     pickImageFromCamera(context: context);
-          //     //add function take Picture
-          //     print('jai pris la photo');
-          //   },
-          //   child: Container(
-          //     height: 120,
-          //     width: 180,
-          //     decoration: BoxDecoration(
-          //         color: Color.fromRGBO(33, 40, 63, 1),
-          //         borderRadius: BorderRadius.circular(17)),
-          //     child: Column(
-          //       mainAxisAlignment: MainAxisAlignment.spaceAround,
-          //       children: [
-          //         Icon(
-          //           Icons.photo_camera,
-          //           color: Color.fromRGBO(142, 142, 147, 1),
-          //           size: 60,
-          //         ),
-          //         Text('Take a picture',
-          //             style: TextStyle(color: Colors.white, fontSize: 23)),
-          //       ],
-          //     ),
-          //   ),
-          // ),
-        ],
-      ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 250,
+              width: 350,
+              padding: EdgeInsets.fromLTRB(20, 10, 0, 10),
+              decoration: BoxDecoration(
+                  color: Theme.of(context).cardColor,
+                  borderRadius: BorderRadius.circular(17)),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text(getText('personalInformation').toString(),
+                      style: TextStyle(color: Colors.white, fontSize: 23)),
+                  Row(
+                    children: [
+                      Text(getText('firstName').toString() + ': Marine',
+                          style: TextStyle(color: Colors.white, fontSize: 20)),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(getText('lastName').toString() + ': Lepen',
+                          style: TextStyle(color: Colors.white, fontSize: 20)),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(getText('username').toString() + ': lamarinade',
+                          style: TextStyle(color: Colors.white, fontSize: 20)),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                          getText('email').toString() +
+                              ': marine.lepen@test.com',
+                          style: TextStyle(color: Colors.white, fontSize: 20)),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            buildLanguage(),
+            buildDarkMode(),
+            // GestureDetector(
+            //   onTap: () {
+            //     pickImageFromCamera(context: context);
+            //     //add function take Picture
+            //     print('jai pris la photo');
+            //   },
+            //   child: Container(
+            //     height: 120,
+            //     width: 180,
+            //     decoration: BoxDecoration(
+            //         color: Color.fromRGBO(33, 40, 63, 1),
+            //         borderRadius: BorderRadius.circular(17)),
+            //     child: Column(
+            //       mainAxisAlignment: MainAxisAlignment.spaceAround,
+            //       children: [
+            //         Icon(
+            //           Icons.photo_camera,
+            //           color: Color.fromRGBO(142, 142, 147, 1),
+            //           size: 60,
+            //         ),
+            //         Text('Take a picture',
+            //             style: TextStyle(color: Colors.white, fontSize: 23)),
+            //       ],
+            //     ),
+            //   ),
+            // ),
+          ],
+        ),
       ),
       bottomNavigationBar: new Footer(page: "profile"),
     );
@@ -168,7 +179,9 @@ class _ProfilePageState extends State<ProfilePage> {
 
   Widget buildLanguage() => DropDownSettingsTile(
         tileColor: Theme.of(context).backgroundColor,
-        leading: IconWidget(icon: Icons.translate_rounded, color: Theme.of(context).accentColor),
+        leading: IconWidget(
+            icon: Icons.translate_rounded,
+            color: Theme.of(context).accentColor),
         settingKey: 'key-language',
         title: getText('language').toString(),
         selected: 1,
@@ -186,18 +199,21 @@ class _ProfilePageState extends State<ProfilePage> {
         tileColor: Theme.of(context).backgroundColor,
         settingKey: 'key-dark-mode',
         title: getText('darkTheme').toString(),
-        leading: IconWidget(icon: Icons.dark_mode, color: Theme.of(context).accentColor),
+        leading: IconWidget(
+            icon: Icons.dark_mode, color: Theme.of(context).accentColor),
         onChange: (isDarkMode) {
           EasyDynamicTheme.of(context).changeTheme();
           setState(() {});
         },
       );
 
-  Future<XFile?> pickImageFromCamera({
+  Future pickImageFromCamera({
     required BuildContext context,
   }) async {
-    final _picker = ImagePicker();
-
-    photoTaken = _picker.pickImage(source: ImageSource.camera);
+    final imagePicker = ImagePicker();
+    final image = await imagePicker.pickImage(source: ImageSource.camera);
+    setState(() {
+      photoTaken = File(image!.path);
+    });
   }
 }
