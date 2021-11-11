a2d7f86efea8f62d549185e0d498dc937216cd57
kodoshi <klivensziu3@gmail.com>
2021-11-11 13:23:30 +0100

    FIX env var on app.js

--- backend/app.js
+++ backend/app.js
@@ -45,5 +45,5 @@ app.use(function (err, req, res, next) {
 });
 
 app.listen(process.env.SERVER_PORT, () => {
-  console.log(`Node server active on port ${process.env.PORT}`);
+  console.log(`Node server active on port ${process.env.SERVER_PORT}`);
 });
